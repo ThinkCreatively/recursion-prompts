cbf38f3e8aabe0607b8c1babe0ec884fe951ff98
James Moore <jameswmoore99@gmail.com>
2021-04-01 13:50:45 -0600

    Finished factorial question and passes all tests

--- src/recursion.js
+++ src/recursion.js
@@ -7,6 +7,22 @@
 // Example: 5! = 5 x 4 x 3 x 2 x 1 = 120
 // factorial(5); // 120
 var factorial = function(n) {
+  //create a result var
+  result = 0;
+  //edge case
+  if (n < 0) {
+    return null;
+  }
+  //base case for when 1 is n
+  if (n === 0) {
+    n = 1;
+    return n * 1;
+  }
+  //recursive case
+  if (n > 0) {
+    result += n * factorial(n - 1);
+  }
+  return result;
 };
 
 // 2. Compute the sum of an array of integers.
